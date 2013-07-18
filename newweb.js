a0fccbd220bdea5cd4360050f3febbdad12927d7
Ubuntu <ubuntu@ip-172-31-29-217.us-west-2.compute.internal>
2013-07-18 04:39:46 +0000

    Updated newindex.html

--- newweb.js
+++ newweb.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-var htmlfile = "index.html";
+var newhtmlfile = "newindex.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var html = fs.readFileSync(htmlfile).toString();
+	var html = fs.readFileSync(newhtmlfile).toString();
 	response.send(html);
 });
 
