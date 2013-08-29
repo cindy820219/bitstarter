396d27e32ce6afdbcd2ec903288e4fff76d05631
cindy820219 <s100321035@mail1.ncnu.edu.tw>
2013-08-29 19:19:25 +0000

    Renew

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
    var fs = require ('fs');
-   var buf = fs.readFileSync('index.html','utf8'); 
+   var buf = fs.readFileSync('copy.html','utf8'); 
    response.send(buf.toString());
 
 
