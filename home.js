5d50350bc83074bd85db80616294d5624e113d27
cindy820219 <s100321035@mail1.ncnu.edu.tw>
2013-08-30 02:56:49 +0000

    home.js

---
+++ home.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+
+   var fs = require ('fs');
+   var buf = fs.readFileSync('home.html','utf8'); 
+   response.send(buf.toString());
+
+
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
